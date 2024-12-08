15482ef368118994bc40d5866931f999bf035d01
abbas148 <ghulamabbas.hsp@gmail.com>
2024-12-08 22:36:53 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+@app.get("/items")
+def read_items():
+    return {"items": ["Item 1", "Item 2", "Item 3"]}
